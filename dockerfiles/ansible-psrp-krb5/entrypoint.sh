b9716298f4550524897c3d0e98e44615718825ef
Ryan Craig <ryan.s.craig@gmail.com>
2021-08-23 23:14:07 -0600

    Update entrypoint.sh

--- dockerfiles/ansible-psrp-krb5/entrypoint.sh
+++ dockerfiles/ansible-psrp-krb5/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-printf "\n### Ansible Controller for secure control of Windows hosts ###\n\n"
+printf "\n######################################################################\n"
+printf "\n# Ansible control node for secure control of Linux and Windows hosts #\n\n"
+printf "\n######################################################################\n"
 
 #/bin/sh
 exec "$@"
